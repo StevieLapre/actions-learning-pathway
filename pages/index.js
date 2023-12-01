5f4bff7558b69de411849c6dbb6bc9f316179e68
StevieLapre <139445870+StevieLapre@users.noreply.github.com>
2023-12-01 17:16:30 +0100

    Update index.js
    JS

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,26 @@ export default function Home() {
   <p>You've completed the essentials of automated application deployment with GitHub Actions! 🥳</p>
 
       </main>
+  const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+
+try {
+  // Fetch the value of the input 'who-to-greet' specified in action.yml
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  
+  // Record the time of greeting as an output
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+
+
+
+} catch (error) {
+  // Handle errors and indicate failure
+  core.setFailed(error.message);
+}
     </>
   )
 }
